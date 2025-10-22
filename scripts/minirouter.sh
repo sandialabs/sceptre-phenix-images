7a1e5721e6715b10517b14ead0f0b87a32a47c2b
cmulk <17730096+cmulk@users.noreply.github.com>
2025-10-22 10:11:16 -0500

    fix(minirouter): enable IP forwarding
    Added command to enable IP forwarding in configuration.

--- scripts/minirouter.sh
+++ scripts/minirouter.sh
@@ -8,3 +8,6 @@ apt remove --purge -y systemd-resolved
 # disable system services, minirouter starts it's own processes
 systemctl disable dnsmasq
 systemctl disable bird
+
+# enable ip forwarding
+echo "net.ipv4.ip_forward=1" > /etc/sysctl.d/11-ip-forwarding.conf
